7486aefed1b37d6e6af9fe3411eb32bed5dd0194
Defdave <igedavid01@gmail.com>
2025-05-07 23:10:18 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  allowedDevOrigins: ['http://172.20.10.14:3000', 'http://localhost:3000'],
 };
 
 export default nextConfig;
